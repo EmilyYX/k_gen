b80ff9b18edecde05c2c48dfa195080cf5546cca
yanxia <yanxia0414@gmail.com>
2018-04-20 08:19:47 +0000

    finish static pages

---
+++ resources/views/layouts/default.blade.php
@@ -0,0 +1,9 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>@yield('title', 'k_gen') - Laravelで構築SNS</title>
+    </head>
+    <body>
+        @yield('content')
+    </body>
+ </html>
---
+++ resources/views/static_pages/about.blade.php
@@ -0,0 +1,5 @@
+@extends('layouts.default')
+@section('title', 'Laravel　SNSについて')
+@section('content')
+  <h1>Laravel　SNSについて</h1>
+@stop
