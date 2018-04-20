b9bc9c1444302a249b9989be77bdb78058430618
yanxia <yanxia0414@gmail.com>
2018-04-20 08:49:55 +0000

    20180420

--- resources/views/layouts/default.blade.php
+++ resources/views/layouts/default.blade.php
@@ -1,9 +1,26 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <title>@yield('title', 'k_gen') - Laravelで構築SNS</title>
-    </head>
-    <body>
-        @yield('content')
-    </body>
- </html>
+  <head>
+    <title>@yield('title', 'k_gen') - Laravelで構築SNS</title>
+    <link rel="stylesheet" href="/css/app.css">
+  </head>
+  <body>
+    <header class="navbar navbar-fixed-top navbar-inverse">
+      <div class="container">
+        <div class="col-md-offset-1 col-md-10">
+          <a href="/" id="logo">k_gen- Laravelで構築SNS</a>
+          <nav>
+            <ul class="nav navbar-nav navbar-right">
+              <li><a href="/help">ヘルプ</a></li>
+              <li><a href="#">ログイン</a></li>
+            </ul>
+          </nav>
+        </div>
+      </div>
+    </header>
+
+    <div class="container">
+      @yield('content')
+    </div>
+  </body>
+</html>
--- resources/views/static_pages/home.blade.php
+++ resources/views/static_pages/home.blade.php
@@ -1,4 +1,16 @@
 @extends('layouts.default')
+
 @section('content')
-  <h1>ホームページ</h1>
+  <div class="jumbotron">
+    <h1>Hello Laravel</h1>
+    <p class="lead">
+       <a href="https://laravel.com/">Laravel </a> で構築SNSのホームページ　by k_gen。
+    </p>
+    <p>
+      始めてみましょうか。
+    </p>
+    <p>
+      <a class="btn btn-lg btn-success" href="#" role="button">新規登録</a>
+    </p>
+  </div>
 @stop
